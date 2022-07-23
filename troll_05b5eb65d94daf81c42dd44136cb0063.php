c815af23b93a9376e8aeaf7eaf4045b54166d558
iw <iwna7777@naver.com>
2022-07-24 08:20:15 +0900

    change deprecated ereg to preg_match

--- troll_05b5eb65d94daf81c42dd44136cb0063.php
+++ troll_05b5eb65d94daf81c42dd44136cb0063.php
@@ -3,7 +3,7 @@
   login_chk(); 
   $conn = dbconnect();  
   if(preg_match('/\'/i', $_GET['id'])) exit("No Hack ~_~");
-  if(@ereg("admin",$_GET['id'])) exit("HeHe");
+  if(@preg_match("/admin/",$_GET['id'])) exit("HeHe");
   $query = "select id from prob_troll where id='{$_GET['id']}'";
   echo "<hr>query : <strong>{$query}</strong><hr><br>";
   $result = @mysqli_fetch_array(mysqli_query($conn,$query)); 
