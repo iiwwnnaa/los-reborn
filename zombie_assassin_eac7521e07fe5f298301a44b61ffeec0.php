216d62675870540b97b80feef2828e8d93c27e81
iw <iwna7777@naver.com>
2022-07-24 08:27:17 +0900

    change deprecated ereg to preg_match

--- zombie_assassin_eac7521e07fe5f298301a44b61ffeec0.php
+++ zombie_assassin_eac7521e07fe5f298301a44b61ffeec0.php
@@ -4,8 +4,8 @@
   $conn = dbconnect();  
   if(preg_match('/\\\|prob|_|\.|\(\)/i', $_GET['id'])) exit("No Hack ~_~"); 
   if(preg_match('/\\\|prob|_|\.|\(\)/i', $_GET['pw'])) exit("No Hack ~_~"); 
-  if(@ereg("'",$_GET['id'])) exit("HeHe"); 
-  if(@ereg("'",$_GET['pw'])) exit("HeHe"); 
+  if(@preg_match("/'/",$_GET['id'])) exit("HeHe"); 
+  if(@preg_match("/'/",$_GET['pw'])) exit("HeHe"); 
   $query = "select id from prob_zombie_assassin where id='{$_GET['id']}' and pw='{$_GET['pw']}'"; 
   echo "<hr>query : <strong>{$query}</strong><hr><br>"; 
   $result = @mysqli_fetch_array(mysqli_query($conn,$query)); 
